7b6d8f53ec252a16285a73cc5b76adabc6e1d354
Lukasz Rokita <lukasz.rokita@allegro.pl>
2022-03-16 15:22:58 +0100

    intensive country grouping

--- reactor/src/test/java/com/nurkiewicz/reactor/R074_GroupingStreams.java
+++ reactor/src/test/java/com/nurkiewicz/reactor/R074_GroupingStreams.java
@@ -1,13 +1,18 @@
 package com.nurkiewicz.reactor;
 
 import com.nurkiewicz.reactor.pagehit.Country;
+import com.nurkiewicz.reactor.pagehit.PageHit;
 import com.nurkiewicz.reactor.pagehit.PageHits;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.GroupedFlux;
 import reactor.util.function.Tuple2;
+import reactor.util.function.Tuples;
+
+import java.time.Duration;
 
 public class R074_GroupingStreams {
 
@@ -19,7 +24,22 @@ public class R074_GroupingStreams {
      */
     @Test
     public void groupByCountryEverySecond() throws Exception {
-        Flux<Tuple2<Country, Long>> hitsPerSecond = null; //TODO
+        Flux<Tuple2<Country, Long>> hitsPerSecond =
+        //        .random()
+        //        .window(Duration.ofSeconds(1))
+        //        .flatMap(
+        //                window -> window.groupBy(PageHit::getCountry).map(it -> it.map())
+        //        );//TODO
+        PageHits.random()
+                .groupBy(PageHit::getCountry)
+                .flatMap(
+                        hitsPerCountry -> hitsPerCountry
+                                .window(Duration.ofSeconds(1))
+                                .flatMap(Flux::count)
+                                .map(sum -> Tuples.of(hitsPerCountry.key(), sum))
+                ).log();
+
+        hitsPerSecond.blockLast();
     }
 
     /**
